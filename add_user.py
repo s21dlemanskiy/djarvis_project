ef653d3937b67cdfcaf8aefa74668b4011936110
koly <koly@djarvisvm.ru-central1.internal>
2022-11-30 22:36:50 +0000

    added test for 'add user'

--- add_user.py
+++ add_user.py
@@ -1,46 +1,60 @@
 import sqlite3
+import subprocess
 
-CONN = sqlite3.connect('Usrs.db')
-
-curs = CONN.cursor()
-curs.execute("""
+def crate_table_if_not_exists(curs):
+    curs.execute("""
             CREATE TABLE IF NOT EXISTS USERS (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 login varchar(128),
                 password_hash INTEGER,
                 work_directory varchar(128)
                 );
-""")
+    """)
 
 
-def add_uesr(login: str, password_hash:int, work_directory:str):
-    cursor.execute('''
+
+def check_login_exist(login: str, curs) -> bool:
+    curs.execute('''
                       SELECT * from Users
                       WHERE login = ?
                   ''', (login, ))
-    if bool(cursor.fetchone()):
+    return bool(curs.fetchone())
+
+def add_user(login: str, password_hash:int, work_directory:str) -> None:
+    if not login or not password_hash or not work_directory:
+        print("empty argument")
+        return 
+
+
+    CONN = sqlite3.connect('Usrs.db')
+    curs = CONN.cursor()
+    crate_table_if_not_exists(curs)
+    
+    login_exist = check_login_exist(login, curs)
+    if login_exist:
         print(f"login '{login}' exists chose another")
         return
-    test = subprocess.check_output(f"sudo bash ./bash_scripts/test_dir_file.sh {work_directory}")
-    if test == "not exist":
+    test = subprocess.check_output(f"sudo bash ./bash_scripts/test_dir_file.sh {work_directory}", shell=True)
+    if test == b"not exist\n":
         print("directory not exist")
-        return
-    elif test == "exist":
+        return 
+    elif test == b"exist\n":
         curs.execute("""
                 INSERT INTO USERS(login, password_hash, work_directory)
                 VALUES(?, ?, ?)
                 """, (login, password_hash, work_directory))
         CONN.commit()
-        print(f"user {login} added"
-        return
+        print(f"user {login} added")
+        return 
     else:
-        print(f"unexpected answer from bash script:{test}"
-        return
+        print(f"unexpected answer from bash script:{test}")
+        return 
 
+def start():
+    login = input("login:")
+    password_hash = hash(input("password:"))
+    work_directory = input("working_directory")
+    add_user(login, password_hash, work_directory)
 
-login = input("login:")
-print()
-password_hash = hash(input("password:"))
-print()
-working_directory = input("working_directory")
-add_user(login, password_hash, work_directory)
+if __name__ == "__main__":
+    start()
---
+++ add_user_test.py
@@ -0,0 +1,76 @@
+import pytest
+import sqlite3
+
+import add_user
+CONN = sqlite3.connect("test.db")
+
+import os
+#start HDFS
+os.system("sudo bash ./bash_scripts/start_hdfs.sh")
+#creating_test_directory
+os.system("sudo bash ./bash_scripts/mk_dir.sh /test")
+os.system("sudo bash ./bash_scripts/mk_dir.sh /test/test")
+
+
+@pytest.fixture(autouse=True)
+def clear_db():
+    print("Cleaning DB")
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS USERS(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            login varchar(128),
+            password_hash INTEGER, 
+            work_directory varchar(128)
+            );
+        """)
+    cursor.execute("""
+    DELETE FROM USERS;
+    """)
+    conn.commit()
+
+
+@pytest.fixture(autouse=True)
+def mock_conn(monkeypatch):
+    monkeypatch.setattr(sqlite3, "connect", lambda _: CONN)
+
+
+
+
+#@pytest.mark.usefixtures("clear_db", "mock_conn")
+@pytest.mark.parametrize("login, password, work_directory", [("test", "test", "/test/test")])
+def test_add_user_normal_values(capsys, login, password, work_directory):
+    add_user.add_user(login, password, work_directory)
+    captured = capsys.readouterr()
+    assert captured.out == f"user {login} added\n"
+
+#@pytest.mark.usefixtures("clear_db", "mock_conn")
+@pytest.mark.parametrize("login, password, work_directory", [("test", "test", "/test/test")])
+def test_user_exist(capsys, login, password, work_directory):
+    add_user.add_user(login, password, work_directory)
+    captured = capsys.readouterr()
+    assert captured.out == f"user {login} added\n"
+    add_user.add_user(login, "password", "work_directory")
+    captured = capsys.readouterr()
+    assert captured.out == f"login '{login}' exists chose another\n"
+
+
+@pytest.mark.parametrize("login, password, work_directory", [("", "test", "/test/test"),
+                                                             ("test", "", "/test/test"),
+                                                             ("test", "test", ""),
+                                                             ("", "", "")])
+def test_empty_data_exist(capsys, login, password, work_directory):
+    add_user.add_user(login, password, work_directory)
+    captured = capsys.readouterr()
+    assert captured.out == "empty argument\n"
+
+
+@pytest.mark.parametrize("login, password, work_directory", [("test", "test", "/test/not_existens"),
+                                                             ("test", "test", "somthin_strange"),
+                                                             ("test", "test", "/wtf/is_it")
+                                                             ])
+def test_non_existent_dir(capsys, login, password, work_directory):
+    add_user.add_user(login, password, work_directory)
+    captured = capsys.readouterr()
+    assert captured.out == "directory not exist\n"
